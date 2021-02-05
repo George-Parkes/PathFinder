fe142c9bd100bd8a6b8e2fe9ca40bdf5043b0eea
george-parkes <george@Georges-MacBook-Pro.local>
2021-02-05 00:10:50 +0000

    added draw barrier function

--- script.js
+++ script.js
@@ -3,27 +3,49 @@ var context = canvas.getContext("2d");
 
 // <-- CREATE Canvas grid with clickable tiles
 
-let squareColor = "yellow";
+const squareColor = "yellow";
+const barrierColor = "purple";
 const outlineColor = "black";
 let cellSize = 20;
 const cols = 20;
 const rows = 20;
+let isHolding = false;
+
+// May need function here to switch TRUE selected tool?
+let toolSelected = {
+  drawBarrier: true,
+  eraseBarrier: false,
+  pointA: false,
+  pointB: false
+}
+
+canvas.width = cellSize * rows;
+canvas.height = cellSize * cols;
 
 // Cell CLASS
 class Cell {
   constructor(posY, posX) {
-    this.position = { x: posX, y: posY },
+    this.position = { x: posX, y: posY }
     this.color = squareColor
+    this.barrierColor = barrierColor
     this.isBarrier = false
   }
   drawCell() {
     // Draw a cell in the grid
     context.beginPath();
+    context.clearRect(       // < -- do I need this?
+      this.position.x * cellSize,
+      this.position.y * cellSize,
+      cellSize, cellSize);
     context.rect(
       this.position.x * cellSize,
       this.position.y * cellSize,
       cellSize, cellSize);
-    context.fillStyle = this.color;
+    if (this.isBarrier) {
+      context.fillStyle = this.barrierColor;
+    } else {
+      context.fillStyle = this.color;
+    }
     context.fill();
     context.strokeStyle= outlineColor;
     context.strokeRect(
@@ -45,11 +67,34 @@ for (let i = 0; i < cellGrid.length; i++) {
   }
 }
 
-// onClick CHANGE square color
+// onClick CHANGE square color 
 
-// set onClick function on each square
-// when clicked
-  // change square color to clickedColor
-  // set isBarrier to TRUE
-  // clear specific Cell
-  // redraw Cell
+if (toolSelected.drawBarrier) {
+  let mX, mY; //position mouseX and mouseY
+
+  canvas.addEventListener('mousedown', e => {
+    mX = e.offsetX;
+    mY = e.offsetY;
+    isHolding = true;
+
+    cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].isBarrier = true;
+    cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].drawCell();
+  });
+
+  canvas.addEventListener('mousemove', e => {
+    if (isHolding === true) {
+      cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].isBarrier = true;
+      cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].drawCell();
+      mX = e.offsetX;
+      mY = e.offsetY;
+    }
+  });
+
+  canvas.addEventListener('mouseup', e => {
+    if (isHolding === true) {
+      cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].isBarrier = true;
+      cellGrid[Math.floor(mY/cellSize)][Math.floor(mX/cellSize)].drawCell();
+      isHolding = false;
+    }
+  });
+}
