4f5aa50d99c28c5a6b50c180a75297873b591f36
george-parkes <george@Georges-MacBook-Pro.local>
2021-02-06 17:00:09 +0000

    cleaned code, added notes, started findPath function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
 const cellSize = 20;
 const cols = 20;
 const rows = 20;
+canvas.width = cellSize * rows;
+canvas.height = cellSize * cols;
 
-const squareColor = "yellow";
+const squareColor = "#f1ffff";
+const outlineColor = "#888";
 const barrierColor = "purple";
-const outlineColor = "black";
+const checkedSquare = "#b3e6ff";
+const pathSquare = "#005580";
 
-// May need function here to switch TRUE selected tool?
+// TOOL SELECTED FUNCTIONALITY ??
 let toolSelected = {
   drawBarrier: true,
   eraseBarrier: false,
@@ -17,27 +21,23 @@ let toolSelected = {
   pointB: false
 }
 
-canvas.width = cellSize * rows;
-canvas.height = cellSize * cols;
-
-// HARD CODE A and B (temp)
+// HARD CODE A and B (TEMP CODE) ////
 const squareA = [cols/2, 1];
-const squareB = [cols/2, rows-1];
+const squareB = [cols/2, rows-2];
 
-// Cell CLASS
+// Cell CLASS CREATOR ////////////////////////
 class Cell {
   constructor(posY, posX) {
     this.position = { x: posX, y: posY }
     this.color = squareColor
     this.barrierColor = barrierColor
     this.isBarrier = false
-
-    if (squareA[0] == posY && squareA[1] == posX) {
-      this.isA = true;
-    } else {
-      this.isA = false   // <-- Best way to set A and B?
-    }
-    this.isB = false
+    this.score = 0
+// Checks if current Cell is A or B and sets true or false
+    squareA[0] == posY && squareA[1] == posX ? 
+      this.isA = true : this.isA = false   // <-- Best way to set A and B?
+    squareB[0] == posY && squareB[1] == posX ? 
+      this.isB = true : this.isB = false
   }
   drawCell() {
     // Draw a cell in the grid
@@ -50,13 +50,17 @@ class Cell {
       this.position.x * cellSize,
       this.position.y * cellSize,
       cellSize, cellSize);
-    if (this.isBarrier) {
+    // Colours cell appropriately
+    if (this.isA) {
+      context.fillStyle = "#66ccff"; // <-- change how colour is applied
+    } else if (this.isB) {
+      context.fillStyle = "green";
+    } else if (this.isBarrier) {
       context.fillStyle = this.barrierColor;
-    } else if (this.isA) {
-      context.fillStyle = "blue";
     } else {
       context.fillStyle = this.color;
     }
+
     context.fill();
     context.strokeStyle= outlineColor;
     context.strokeRect(
@@ -67,7 +71,7 @@ class Cell {
   }
 }
 
-// <-- Create GRID
+// <-- Create GRID on screen //////////////////
 let cellGrid = new Array(cols); // y
 
 for (let i = 0; i < cellGrid.length; i++) {
@@ -78,16 +82,17 @@ for (let i = 0; i < cellGrid.length; i++) {
   }
 }
 
+///// DRAW BARRICADES //////////////////////////////
 let mX, mY; //position mouseX and mouseY GLOBAL
 let isHolding = false;
 
-function mousePositionOnGrid () { // SETS MOUSE POSITION
+function mousePositionOnCanvas () { // SETS MOUSE POSITION
   canvas.onmousemove = function(e) {
     mX = Math.floor(e.offsetX/cellSize);
     mY = Math.floor(e.offsetY/cellSize);
   }
 }
-mousePositionOnGrid();
+mousePositionOnCanvas();
 
 if (toolSelected.drawBarrier) { // <-- DRAW BARRIER
   // MOUSE DOWN
@@ -112,3 +117,36 @@ if (toolSelected.drawBarrier) { // <-- DRAW BARRIER
     }
   });
 }
+
+// PATH FINDER FUNCTION ////////////
+// from SquareA(or start?)
+  // find direction to SquareB
+  // check if squares in that direction are free
+  // if (square are available | isBarrier == false) {
+    // move to that square
+    // set square to pathSquare
+  // } else {
+    // check other next best square
+    // if that's available move there
+    // else go back along along route to next available square
+  // }
+
+  // allocate score to each square based on dx/dy when compared with its position/squareB pos
+
+const findPath = () => {
+  // find direction of squareB
+  let currentCell = cellGrid[squareA[0]][squareA[1]];
+  let finalCell = cellGrid[squareB[0]][squareB[1]];
+  let availableSquares = [];
+
+  let dx = finalCell.position.x - currentCell.position.x;
+  let dy = finalCell.position.y - currentCell.position.y;
+
+  // check surrounding squares
+  for (let i = -1; i <= 1; i++) {
+    currentCell[0 + i, 1].isBarrier;
+
+  }
+}
+
+findPath();
